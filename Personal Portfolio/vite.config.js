f7f22fe6fcb3f9299b007b7ebad5f538b8eed8e9
Tanzeela Memon <tanzeelamemon.bscsf22@iba-suk.edu.pk>
2025-07-03 19:32:55 +0500

    Personal Portfolio Landing Page

--- Personal Portfolio/vite.config.js
+++ Personal Portfolio/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [ tailwindcss(), react() ],
-  base: './'
-})
+  plugins: [tailwindcss(),react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
